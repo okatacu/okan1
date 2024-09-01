f1d4d1a87421df4180c8fa60477b1ba3a57da4dd
okatacu <okatacu@gmail.com>
2024-09-01 19:37:26 +0300

    update 1

---
+++ Abuzittin.java
@@ -0,0 +1,65 @@
+
+public class Abuzittin {
+
+	static double valuedeneme = 5; // Global Scope
+
+	public static void main(String[] args) {
+
+		System.out.println(valuedeneme);
+		System.out.println("Hello World1 !!!");
+		System.out.println("Hello World2 !!!");
+		System.out.println("Hello World3 !!!");
+		System.out.println("Hello World4 !!!");
+		System.out.println("Hello World5 !!!");
+		System.out.printl("Abuzittin");
+
+	/*	for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World !!" + i);
+			if (i == 2) {
+				break;
+			}
+		}
+
+		boolean type1 = true; // true false
+		char type2 = 's'; // 16 bit // karakterler a b c 1 2 4 *
+		byte type3 = 1; // 8 bit 0x12
+		short type4 = 2; // 16 bit 0xFF
+		int type5 = 2147483647; // 32 bit = 4 byte 10 20 32 -2147483648 to 2147483647
+		long type6 = 2147483648l; // 64 bit 10 20 25 32
+		float type7 = 3954151513242333333333333423515151.95f; // 32 bit 21.52 -32.98
+		double type8 = 34; // 64 bit 325415151515151.95
+
+		String type9 = "abuzittin";  
+		char[] type10= {'a','b','u','z','i'};
+		
+		for (int i = 0; i < type9.toCharArray().length; i++) {  // LOOP
+			System.out.println(type9.charAt(i));
+		}
+
+		for (int i = 0; i < type10.length; i++) {
+			System.out.println(type10[i]);
+		}
+
+		// Garbage Collector
+		// Scope
+		{
+			System.out.println(valuedeneme);
+			double var = 10;
+			System.out.println(var);
+			var = 20;
+			if (true) { //CONDITIONS
+				var = 30;
+				System.out.println(var);
+			}
+			System.out.println(var);
+		}
+		System.out.println(valuedeneme);
+//		System.out.println(var); // var out of scope
+
+		// Double asd = new Double(type8);
+		// Float
+		// Integer
+*/
+	}
+
+}
